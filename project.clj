d8e0cae4dbb934146166fd40cc369ece6efcac07
Jon Staab <shtaab@gmail.com>
2018-12-21 07:28:26 -0800

    Adding project.clj for heroku

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject bedug "1.0.0-SNAPSHOT"
+  :description "A programming game for kids"
+  :url "http://bedug.herokuapp.com"
+  :license {:name "MIT"}
+  :dependencies [[http-kit "2.3.0"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
+  :profiles {:production {:env {:production true}}})
